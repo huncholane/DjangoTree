a2ee8aad4a308453fa29cb7eee675a89ce229278
Lane Henslee <huncho@hygo.ai>
2024-05-16 12:36:51 -0500

    able to use inDjangoProject

--- src/context.ts
+++ src/context.ts
@@ -1,13 +1,38 @@
 import * as vscode from "vscode";
 import * as path from "path";
+import * as fs from "fs";
 
-export const setDjangoContext = () => {
-  vscode.workspace.findFiles("manage.py", "", 1).then((files) => {
-    let isDjangoProject = files.length > 0;
-    vscode.commands.executeCommand(
-      "setContext",
-      "isDjangoProject",
-      isDjangoProject
-    );
-  });
+export const setDjangoContext = async () => {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (workspaceFolders) {
+    for (const folder of workspaceFolders) {
+      const dir = folder.uri.fsPath;
+      const isDjango = await isDjangoProject(dir);
+      const inDjango = isDjango || (await isInDjangoProject(path.dirname(dir)));
+      vscode.commands.executeCommand("setContext", "isDjangoProject", isDjango);
+      vscode.commands.executeCommand("setContext", "inDjangoProject", inDjango);
+    }
+  } else {
+    vscode.commands.executeCommand("setContext", "isDjangoProject", false);
+    vscode.commands.executeCommand("setContext", "inDjangoProject", false);
+  }
+};
+
+const isDjangoProject = async (dir: string): Promise<boolean> => {
+  const managePyPath = path.join(dir, "manage.py");
+  return fs.existsSync(managePyPath);
+};
+
+const isInDjangoProject = async (dir: string): Promise<boolean> => {
+  const managePyPath = path.join(dir, "manage.py");
+  if (fs.existsSync(managePyPath)) {
+    return true;
+  }
+
+  const parentDir = path.dirname(dir);
+  if (parentDir === dir) {
+    return false;
+  }
+
+  return isInDjangoProject(parentDir);
 };
